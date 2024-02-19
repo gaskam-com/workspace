be87904c0aead5f85bc1e91e9c5c3a598e13224d
PatafixPLTX <gaspard.lebaube@gaskam.com>
2024-02-19 18:57:05 +0100

    Update .gitignore and deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
+@echo off
+clear
+echo "Deploying..."
+echo "Installing bun: "
 curl -fsSL https://bun.sh/install | bash
+echo "Installing dependencies: "
 bun i
+echo "Set environment variables: "
 bun run deploy
